3c32646e251c73e77ed351ba174a290341ceb637
Daniel Carlsson <dc222bz@student.lnu.se>
2022-10-15 14:59:50 +0200

    Add logic for exercises

--- src/js/index.js
+++ src/js/index.js
@@ -9,3 +9,103 @@ import './components/my-inputform/index.js'
 import './components/my-bmr/index.js'
 import './components/my-maintenancecalories/index.js'
 import './components/my-exercises/index.js'
+
+document
+  .querySelector('my-inputform')
+  .addEventListener('inputform', (event) => {
+    const test = document.querySelector('my-exercises')
+    if (test !== 'undefined' && test !== null) {
+      document.querySelectorAll('my-exercises').forEach((e) => e.remove())
+    }
+    const activitylevel = window.sessionStorage.getItem(
+      'myapp-activitylevel'
+    )
+    switch (activitylevel) {
+      case '1.2':
+        document.querySelectorAll('my-exercises').forEach((e) => e.remove())
+        break
+      case '1.4':
+        document
+          .querySelector('#exercises')
+          .appendChild(document.createElement('my-exercises'))
+        document
+          .querySelector('#exercises')
+          .appendChild(document.createElement('my-exercises'))
+        break
+      case '1.6':
+        document
+          .querySelector('#exercises')
+          .appendChild(document.createElement('my-exercises'))
+        document
+          .querySelector('#exercises')
+          .appendChild(document.createElement('my-exercises'))
+        document
+          .querySelector('#exercises')
+          .appendChild(document.createElement('my-exercises'))
+        break
+      case '1.75':
+        document
+          .querySelector('#exercises')
+          .appendChild(document.createElement('my-exercises'))
+        document
+          .querySelector('#exercises')
+          .appendChild(document.createElement('my-exercises'))
+        document
+          .querySelector('#exercises')
+          .appendChild(document.createElement('my-exercises'))
+        document
+          .querySelector('#exercises')
+          .appendChild(document.createElement('my-exercises'))
+        document
+          .querySelector('#exercises')
+          .appendChild(document.createElement('my-exercises'))
+        break
+      case '2.0':
+        document
+          .querySelector('#exercises')
+          .appendChild(document.createElement('my-exercises'))
+        document
+          .querySelector('#exercises')
+          .appendChild(document.createElement('my-exercises'))
+        document
+          .querySelector('#exercises')
+          .appendChild(document.createElement('my-exercises'))
+        document
+          .querySelector('#exercises')
+          .appendChild(document.createElement('my-exercises'))
+        document
+          .querySelector('#exercises')
+          .appendChild(document.createElement('my-exercises'))
+        document
+          .querySelector('#exercises')
+          .appendChild(document.createElement('my-exercises'))
+        document
+          .querySelector('#exercises')
+          .appendChild(document.createElement('my-exercises'))
+        break
+      case '2.4':
+        document
+          .querySelector('#exercises')
+          .appendChild(document.createElement('my-exercises'))
+        document
+          .querySelector('#exercises')
+          .appendChild(document.createElement('my-exercises'))
+        document
+          .querySelector('#exercises')
+          .appendChild(document.createElement('my-exercises'))
+        document
+          .querySelector('#exercises')
+          .appendChild(document.createElement('my-exercises'))
+        document
+          .querySelector('#exercises')
+          .appendChild(document.createElement('my-exercises'))
+        document
+          .querySelector('#exercises')
+          .appendChild(document.createElement('my-exercises'))
+        document
+          .querySelector('#exercises')
+          .appendChild(document.createElement('my-exercises'))
+        break
+      default:
+    }
+  })
